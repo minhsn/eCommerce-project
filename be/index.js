33a059524ff90ec702fade926d0ae7723290a14e
minh <minhsn89@gmail.com>
2023-08-15 23:29:52 +0700

    init-project

---
+++ be/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors({
+    origin : process.env.CLIENT_URL,
+    methods : ['GET', 'POST', 'PUT', 'DELETE']
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.use('/', (req, res) => {
+    return res.send('server on')
+})
+
+const PORT = process.env.PORT || 8888
+
+const listener = app.listen(PORT, () => {
+    console.log(`server run on port ${listener.address().port}`)
+})
