208c73fb8ba0c14cd905ac75e310dc33b2165f71
minh <minhsn89@gmail.com>
2023-08-29 00:56:15 +0700

    update header

--- fe/src/Layout/DefaultLayout/Header/index.js
+++ fe/src/Layout/DefaultLayout/Header/index.js
@@ -1,14 +1,25 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom'
 
 const cx = classNames.bind(styles)
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('content')}>
-
+    return (<header className={cx('wrapper')}>
+        <div className={cx('nav')}>
+            <ul className={cx('nav-list')}>
+                <li className={cx('nav-item')}>
+                    <Link to='/' class="navbar-brand" ><img src={'logo/logo.png'} class={cx('logo')} alt='logo' /></Link>
+                </li>
+                <li className={cx('nav-item')}>menu</li>
+                <li className={cx('nav-item')}>order</li>
+            </ul>
+            <ul className={cx('nav-list')}>
+                <li className={cx('nav-item')}>login</li>
+                <li className={cx('nav-item')}>register</li>
+            </ul>
         </div>
-    </header>;
+    </header> )
 }
 
 export default Header;
