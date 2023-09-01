13a903d7b27984f71ee5e57ce6e6137dd15edd6c
minh <minhsn89@gmail.com>
2023-09-01 21:46:13 +0700

    update header layout

--- fe/src/Layout/DefaultLayout/Header/index.js
+++ fe/src/Layout/DefaultLayout/Header/index.js
@@ -1,22 +1,36 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom'
+import { Cookies } from 'react-cookie';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const cookies= new Cookies()
+    const [token, setToken] = useState(cookies.get('token'))
+
+
+
     return (<header className={cx('wrapper')}>
         <div className={cx('nav')}>
             <ul className={cx('nav-list')}>
                 <li className={cx('nav-item')}>
-                    <Link to='/' class="navbar-brand" ><img src={'logo/logo.png'} class={cx('logo')} alt='logo' /></Link>
+                    <Link to='/' className={cx("navbar-brand")} ><img src={'logo/logo.png'} className={cx('logo')} alt='logo' /></Link>
                 </li>
-                <li className={cx('nav-item')}>menu</li>
-                <li className={cx('nav-item')}>order</li>
             </ul>
-            <ul className={cx('nav-list')}>
-                <li className={cx('nav-item')}>login</li>
-                <li className={cx('nav-item')}>register</li>
+            <ul className={cx('nav-list')} style={{display: token? 'none': 'block'}}>
+                <li className={cx('nav-item')}>
+                    <Link to='/login' className={cx('link-button')}>Login</Link>
+                </li>
+                <li className={cx('nav-item')}>
+                    <Link to='/register' className={cx('link-button')}>Register</Link>
+                </li>
+            </ul>
+            <ul className={cx('nav-list')} style={{display: !token? 'none': 'block'}}>
+                <li className={cx('nav-item')}>
+                    <Link to='/login' >avatar</Link>
+                </li>
             </ul>
         </div>
     </header> )
